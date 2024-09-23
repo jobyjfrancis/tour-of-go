f41119c6ff93c3022314a38c7c23252a80b9dc5e
Joby Francis <joby.francis@silverstripe.com>
2024-09-23 18:30:29 +1200

    generics

--- generics/main.go
+++ generics/main.go
@@ -15,6 +15,7 @@ func main() {
 	}
 
 	fmt.Printf("Non-generic sums: %v and %v\n", SumInts(ints), SumFloats(floats))
+	fmt.Printf("Generic sums: %v and %v\n", SumIntsOrFloats[string, int64](ints), SumIntsOrFloats[string, float64](floats))
 }
 
 // SumInts adds together the values of m.
@@ -29,6 +30,14 @@ func SumInts(m map[string]int64) int64 {
 // SumFloats adds together the values of m.
 func SumFloats(m map[string]float64) float64 {
 	var s float64
+	for _, v := range m {
+		s += v
+	}
+	return s
+}
+
+func SumIntsOrFloats[K comparable, V int64 | float64](m map[K]V) V {
+	var s V
 	for _, v := range m {
 		s += v
 	}
